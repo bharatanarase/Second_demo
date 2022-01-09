d6e9cd7412f68d1cbd18300f01b1cb6a6ae6a846
bharatanarase66@gmail.com <bharatanarse66@gmail.com>
2022-01-09 20:02:29 +0530

    Welocome to scound demo

---
+++ src/main/java/demo_second/DemoSecondApplication.java
@@ -0,0 +1,13 @@
+package demo_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecondApplication.class, args);
+	}
+
+}
